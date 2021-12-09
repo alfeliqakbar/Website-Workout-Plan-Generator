269c62a3570e4c0b07090cf867974c4e63592004
Al Feliq Akbar <alfeliqakbar@gmail.com>
2021-12-10 02:58:01 +0700

    membuat routing dynamic page

---
+++ src/scripts/views/pages/workout-plan.js
@@ -0,0 +1,13 @@
+const WorkoutPlan = {
+  async render() {
+    return `
+      <h1>Workout Plan Page</h1>
+    `;
+  },
+
+  async afterRender() {
+    // fungsi yang dipanggil saat setelah page muncul jalan di dalam sini
+  },
+};
+
+export default WorkoutPlan;
