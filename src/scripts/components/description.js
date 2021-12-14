7e4d094b60399fc1207ea5908c12beec84952b4c
Mochammad Maulana <mumetofficial@gmail.com>
2021-12-14 13:27:41 +0700

    Memperbaiki Navbar + About Page

--- src/scripts/components/description.js
+++ src/scripts/components/description.js
@@ -7,17 +7,23 @@ class describeWeb extends HTMLElement {
     this.innerHTML = `
     
         <div class="row p-3">
-        <h2>Tujuan</h2>
+        <h2>Purpose</h2>
         <div class="col-md-6">
             <img src="/assets/hero.jpg" class="img-fluid" alt="">
         </div>
         <div class="col-md-6">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, id delectus eius enim accusantium molestias. Molestiae pariatur laborum dignissimos possimus.</p>
+            <p style="font-size:1.2em;text-align:justify">This website was created to help everyone live healthier and exercise with tools that are easy to find and easy to use. thereby strengthening the immune system and preventing various diseases.</p>
         </div>
         </div>
         <div class="row mt-3 p-3">
-        <h2>Anjass</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, tempore.</p>
+        <h2>The advantages of this website</h2>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fs-5"><i class="far fa-check-circle" style="color:green"></i> Free To Use</li>
+          <li class="list-group-item fs-5"><i class="far fa-check-circle" style="color:green"></i> Easy to understand</li>
+          <li class="list-group-item fs-5"><i class="far fa-check-circle" style="color:green"></i> Fast and save memory</li>
+          <li class="list-group-item fs-5"><i class="far fa-check-circle" style="color:green"></i> Supports various devices</li>
+          <li class="list-group-item fs-5"><i class="far fa-check-circle" style="color:green"></i> Care about your health</li>
+        </ul>
         </div>
     
     `;
--- src/scripts/components/our-team.js
+++ src/scripts/components/our-team.js
@@ -8,26 +8,47 @@ class ourTeam extends HTMLElement {
     
         <h2>Our Team</h2>
         <div class="row d-flex justify-content-center mb-3">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+        <div class="card p-3" style="width: 18rem;">
+            <img src="/assets/profil-team.jpg" class="rounded-circle mx-auto" alt="..." style="width:200px">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">Al Feliq Akbar</h5>
+            <p class="card-text">Universitas.</p>
+            <ul class="list-unstyled list-inline">
+                <li class="list-inline-item">
+                    <a href="#" class="btn-floating text-black btn-sm fs-2"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="#" class="btn-floating text-black btn-sm fs-2"><i class="fab fa-github"></i></a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="#" class="btn-floating text-black btn-sm fs-2"><i class="fab fa-linkedin"></i></a>
+                </li>
+            </ul>
             </div>
         </div>
         </div>
 
         <div class="row d-flex justify-content-center mb-3">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+        <div class="card p-3" style="width: 18rem;">
+            <img src="/assets/profil-team.jpg" class="rounded-circle mx-auto" alt="..." style="width:200px">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">Mochammad Maulana</h5>
+            <p class="card-text">Universitas Jember.</p>
+            <ul class="list-unstyled list-inline">
+                <li class="list-inline-item">
+                    <a href="#" class="btn-floating text-black btn-sm fs-2"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="#" class="btn-floating text-black btn-sm fs-2"><i class="fab fa-github"></i></a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="#" class="btn-floating text-black btn-sm fs-2"><i class="fab fa-linkedin"></i></a>
+                </li>
+            </ul>
             </div>
         </div>
         </div>
+
     
     `;
   }
