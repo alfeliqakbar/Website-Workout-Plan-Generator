bbfbebde6a1cc2bf0edd11e793623a665986cb27
Al Feliq Akbar <alfeliqakbar@gmail.com>
2021-12-22 14:21:52 +0700

    akhirnya berhasil menampilkan object yang berubah-ubah

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,13 +1,13 @@
 import CONFIG from './config';
 
 const API_ENDPOINT = {
-  WORKOUT_PLAN_GENERATOR: ({
-    type,
-    area,
-    level,
-    duration,
-  }) => `${CONFIG.BASE_URL}?type=${type}&area=${area}&level=${level}&duration=${duration}`,
-  // WORKOUTPLAN: `${CONFIG.BASE_URL}?type=strength&area=full&level=beginner&duration=30`,
+  // WORKOUT_PLAN_GENERATOR: ({
+  //   type,
+  //   area,
+  //   level,
+  //   duration,
+  // }) => `${CONFIG.BASE_URL}?type=${type}&area=${area}&level=${level}&duration=${duration}`,
+  WORKOUTPLAN: `${CONFIG.BASE_URL}?type=strength&area=full&level=beginner&duration=30`,
 };
 
 export default API_ENDPOINT;
